2fd47cdfbdefdd2db2cc11d0c5792a3578d16564
Benran Zhang <jimyzhan@usc.edu>
2023-01-18 17:44:28 -0800

    wave initial page

--- src/pages/projects/wave/Wave.js
+++ src/pages/projects/wave/Wave.js
@@ -26,12 +26,13 @@ import styles from './Wave.module.css';
 
 const title = 'Wave: an interactive musical dating application.';
 const description =
-  'I lead an engineering team of 6 people to build this dating application to address the stale ';
+  'I lead an team of 7 fellow students to build this dating application to address the stale and unimaginative state of other dating applications.';
 const roles = [
-  'Backend Developer',
-  'Backend/Frontend Integration',
-  'Front End Development',
-  'Deployment and Environment Standarization',
+  'Project Management',
+  'System Design',
+  'Distributed Systems',
+  'Supervised Learning',
+  'IOS Application',
 ];
 
 export const Wave = () => {
@@ -55,53 +56,32 @@ export const Wave = () => {
           url="https://hacksc.com/"
           roles={roles}
         />
-        <ProjectSection padding="top">
-          <ProjectSectionContent>
-            <ProjectImage
-              raised
-              key={themeId}
-              srcSet={[HackSCMainPage, HackSCMainPage]}
-              placeholder={[HackSCMainPage, HackSCMainPage]}
-              sizes={`(max-width: ${media.mobile}px) 100vw, (max-width: ${media.tablet}px) 800px, 1000px`}
-            />
-          </ProjectSectionContent>
-        </ProjectSection>
         <ProjectSection>
           <ProjectTextRow>
             <ProjectSectionHeading>Project Intiative</ProjectSectionHeading>
             <ProjectSectionText>
-              HackSC was founded in USC with the purpose of providing better, more
-              engaging, and more functional tools for hosting hackathons. With this
-              mission we have hosted three hackathons since 2020 and now aim to open
-              source all the tools that we use in planning and hosting a hackathon. By
-              imporving scalability of our management systems, building an SSO system from
-              scratch, and adding to the evergrowing functionality of our platform,
-              project Hibiscus will provide a accessible platform for anyone to host a
-              hackathon.
+              We founded Wave at USC with a group of like-minded, diversed group of
+              students who belives in the power of music and how much music affects
+              relationships. We decided to take the idea of music and dating to the next
+              level by building an ios application that fully integrates music into every
+              functionality to create a unique and inspiring user experience.
             </ProjectSectionText>
           </ProjectTextRow>
         </ProjectSection>
         <ProjectSection light={isDark}>
           <ProjectSectionContent>
-            <Image
-              key={themeId}
-              srcSet={[HackSCAdminUI, HackSCAdminUI]}
-              placeholder={[HackSCAdminUI, HackSCAdminUI]}
-              alt={`A set of ${themeId} themed components for the aero design system`}
-              sizes="100vw"
-            />
             <ProjectTextRow>
-              <ProjectSectionHeading>
-                The admin portal and functionalities
-              </ProjectSectionHeading>
+              <ProjectSectionHeading>Project and System Structure</ProjectSectionHeading>
               <ProjectSectionText>
-                Personally, I worked on adding and perfecting functions to the
-                administrator portal of project Hibiscus. Using Next.js 13, our
-                engineering team were able to create a responsive ui with the
-                functionality of organizing teams in a hackathon. Through the hacker
-                portal, hackers can create, join, or request to be part of a team. By
-                using routing and SSR techiniques in Next.js and Express, these
-                functionalities can be easily repurposed for an open sourced project.
+                Because of the user-oriented nature of the application, we designed the
+                entire project structure to be extremly scalable and capable of handeling
+                extremly heavy server load. Because of theis, we implemented a
+                functiona-based distributed system that can split load across different
+                instances to produce a fast, responsive, and reliable tech stack. We also
+                choose an layered structure for this project. We have layers that deal
+                with compartimentalized sections of the application such as: service
+                logic, machine learning, recommendation, and kafka flow and real time
+                data-synchronization.
               </ProjectSectionText>
             </ProjectTextRow>
           </ProjectSectionContent>
@@ -110,78 +90,22 @@ export const Wave = () => {
         <ProjectSection>
           <ProjectSectionContent>
             <ProjectTextRow>
-              <ProjectSectionHeading>
-                Log Server and Hacker Matching
-              </ProjectSectionHeading>
+              <ProjectSectionHeading>Service Logic and Database</ProjectSectionHeading>
               <ProjectSectionText>
-                Among our many applications in project Hibiscus, the log server stands as
-                an unique BaaS product that provides tracking for any event
-                organizization. By using milliesearch and express.js, we cosntructed a
-                standalone backend service that offers customizablecontent tracking that
-                is compatible with all major frontend frameworks. Using milliesearch and
-                docker containment, we also provide a easy automatic deployment image
-                generator that can automatically deploy project updates without server
-                side image rebuilding.
-              </ProjectSectionText>
-            </ProjectTextRow>
-          </ProjectSectionContent>
-        </ProjectSection>
-
-        <ProjectSection light={isDark}>
-          <ProjectSectionContent>
-            <Image
-              key={themeId}
-              srcSet={[HackSCStorybook, HackSCStorybook]}
-              placeholder={[HackSCStorybook, HackSCStorybook]}
-              sizes="100vw"
-            />
-            <ProjectTextRow>
-              <ProjectSectionHeading>UI Components and Storybook</ProjectSectionHeading>
-              <ProjectSectionText>
-                To help others adopt our paltforms, we created a storybook for individual
-                components of our platform where users can browse through each component
-                template that we used in both our recruitment page and our UI for hacker
-                portal and administrator portal. These elements are also
-                compartimentalized for easy usage outside of the Hibiscus project.
-              </ProjectSectionText>
-            </ProjectTextRow>
-          </ProjectSectionContent>
-        </ProjectSection>
-
-        <ProjectSection>
-          <ProjectSectionContent>
-            <ProjectTextRow>
-              <ProjectSectionHeading>Standarization and Deployment</ProjectSectionHeading>
-              <ProjectSectionText>
-                During the deployment phase of project Hibiscus, I was also part of the
-                team that standarized all our various applications and backend services
-                using Docker images and constructed customized image containers for safe
-                and universal deployment. Most of our application use the Next.js
-                framework and are deployed using the vercel webpack manager and deployment
-                tool. We also created automated scirpts for image generation to aid others
-                in deploying their projects if they based their systems on project
-                Hibiscus.
-              </ProjectSectionText>
-            </ProjectTextRow>
-          </ProjectSectionContent>
-        </ProjectSection>
-
-        <ProjectSection light={isDark}>
-          <ProjectSectionContent>
-            <ProjectTextRow center centerMobile noMargin>
-              <ProjectSectionHeading>Project outcomes</ProjectSectionHeading>
-              <ProjectSectionText>
-                Overall our student organization is one of the largest hackathon orgs in
-                the US and we are continueing to host the HackSC 2023 hackathon this
-                Feburary. We released project Hibiscus in December 2022 and will continue
-                to contribute to the open source project as we enhance our hosting
-                functionalities and platforms.
+                The backend of the project serves as a concentration of all other layers
+                and functionalities and encapsulats them in such a way that can be called
+                by our ios frontend. We used Spring Boot for our project since it offers a
+                very versitile functionality that is compatible out of the box with most
+                of our other layers. For our database, since the nature of the data that
+                we deal with is user based and would not benefit from a tabled structure,
+                we devcided to use DynamoDB to handle user information and profile. Our
+                Spring layer directly manipulates our database to carry out CRUD
+                functionalities for basic functions.
               </ProjectSectionText>
             </ProjectTextRow>
           </ProjectSectionContent>
         </ProjectSection>
       </ProjectContainer>
-      <Footer />
     </Fragment>
   );
 };
