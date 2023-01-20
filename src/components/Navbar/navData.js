dc819770e2a336e4027b91b6b9c9e2b4c9d967e8
Benran Zhang <jimyzhan@usc.edu>
2023-01-19 21:06:19 -0800

    chinese character replacement

--- src/components/Navbar/navData.js
+++ src/components/Navbar/navData.js
@@ -23,18 +23,8 @@ export const navLinks = [
 
 export const socialLinks = [
   {
-    label: 'Twitter',
-    url: 'https://twitter.com/hamishMW',
-    icon: 'twitter',
-  },
-  {
-    label: 'Figma',
-    url: 'https://www.figma.com/@Hamish',
-    icon: 'figma',
-  },
-  {
     label: 'Github',
-    url: 'https://github.com/HamishMW',
+    url: 'https://github.com/JimyZ13',
     icon: 'github',
   },
 ];
